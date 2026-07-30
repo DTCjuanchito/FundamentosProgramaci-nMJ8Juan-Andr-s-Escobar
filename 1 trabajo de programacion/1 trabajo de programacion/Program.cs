8e87cc100411207cf984acb0c4493c842f5117e5
DTCjuanchito <juana.escobarv@upb.edu.co>
2026-07-30 08:15:11 -0500

    Primer commit

---
+++ 1 trabajo de programacion/1 trabajo de programacion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace _1_trabajo_de_programacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "ferney";
+            Console.WriteLine("bienvenido al curso de fundamentos de programacion");
+        }
+    }
+}
