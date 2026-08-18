93e4e4d68346a1f823205c8a726b506078c79972
DTCjuanchito <juana.escobarv@upb.edu.co>
2026-08-18 09:39:36 -0500

    7.tallerRepasoCondicionales

---
+++ 7.TallerrepasoCondicionales/7.TallerrepasoCondicionales/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.TallerrepasoCondicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int munición;
+            bool invencible;
+
+            cout<<
+
+
+            //primner ejercicio
+            /*    decimal numero1;
+                decimal numero2;
+                decimal numero3;
+
+                Console.WriteLine("Ingrese el numero1");
+                numero1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Ingrese el numero2");
+                numero2 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Ingrese el numero3");
+                numero3 = Convert.ToDecimal(Console.ReadLine());
+
+                if(numero1 == numero2 || numero1==numero3 || numero2==numero3)
+                {
+                    Console.WriteLine("Los nuymeros deben ser diferentes");
+                }
+                else
+                {
+                    Console.WriteLine("Los tres numeros son diferentes");
+
+                    if (numero1 > numero2)
+                    {
+                        if (numero1 > numero3)
+                        {
+                            Console.WriteLine("El numero 1 es el mayor");
+                            if (numero2>numero3)
+                            {
+                                Console.WriteLine("El numero 2 es el del medio");
+                                Console.WriteLine("El numero 3 es el menor");
+                            }
+                            else
+                            {
+                                Console.WriteLine("El numero 3 es el del medio");
+                                Console.WriteLine("El numero 2 es el menor");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("El numero 3 es el mayor");
+                            Console.WriteLine("El numero 1 es el del medio");
+                            Console.WriteLine("El numero 2 es el menor");
+                        }
+
+                    }
+                    else
+                    {
+                        if (numero2 > numero3)
+                        {
+                            Console.WriteLine("El numero 2 es el mayor");
+                            if (numero1 > numero3)
+                            {
+                                Console.WriteLine("El numero 1 es el del medio");
+                                Console.WriteLine("El numero 3 es el menor");
+                            }
+                            else
+                            {
+                                Console.WriteLine("El numero 3 es el del medio");
+                                Console.WriteLine("El numero 1 es el menor");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("El numero 3 es el mayor");
+                            Console.WriteLine("El numero 2 es el del medio");
+                            Console.WriteLine("El numero 1 es el menor");
+                        }
+
+                    }
+                }
+                   */
+        }
+    }
+}
