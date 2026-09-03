01201fc7022390ccb174b5df7295cb46fda6c135
DTCjuanchito <juana.escobarv@upb.edu.co>
2026-09-03 09:26:21 -0500

    13.CiclosFor

---
+++ 13.CiclosFor/13.CiclosFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.CiclosFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int contador = 1; contador<=5;contador++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine($"La suma de los cinco numeros es:{acumulador}");
+        }
+    }
+}
